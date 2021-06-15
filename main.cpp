7b576127f74d1924d56453aadb40a6adf9777883
ok-sana-ko <73507364+ok-sana-ko@users.noreply.github.com>
2021-06-15 10:20:08 +0300

    Update main.cpp
    added unit tests for the program

--- main.cpp
+++ main.cpp
@@ -1,30 +1,69 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
-void calc(string FileName)
+string calc(string FileName)
 {
-    ifstream fout(FileName);
+    ifstream fin(FileName);
     char temp;
     int N9 = 0;
     int N0 = 0;
     int N1 = 0;
-    while (fout >> temp)
+    string res = "";
+
+    while (fin >> temp)
     {
         if (temp == '9') N9++;
         if (temp == '0') N0++;
         if (temp == '1') N1++;
     }
-    cout << "0x" << N0 << " ";
-    cout << "1x" << N1 << " ";
-    cout << "9x" << N9 << " ";
+    
+    res += "0x" + to_string(N0);
+    res += " ";
+    res += "1x" + to_string(N1);
+    res += " ";
+    res += "9x" + to_string(N9);
+    fin.close();
+    return res;
+}
+
+class MyTest
+{
+public:
+    void Test1();
+    void Test2();
+    void Test();
+};
+void MyTest::Test1()
+{
+    ofstream fout("test1.txt");
+    fout << "0" << endl << "11" << endl << "999" << endl;
+    if (calc("test1.txt") != "0x1 1x2 9x3") cout << "Test1 NOT OK" << endl;
+    else cout << "Test1 OK" << endl;
+    fout.close();
+}
+void MyTest::Test2()
+{
+    ofstream fout("test2.txt");
+    fout << "0" << endl << "11" << endl << "99" << endl << "9"<<endl;
+    if (calc("test2.txt") != "0x1 1x2 9x3") cout << "Test2 NOT OK" << endl;
+    else cout << "Test2 OK" << endl;
+    fout.close();
+}
+void MyTest::Test()
+{
+    Test1();
+    Test2();
 }
 
 int main()
 {
     string FileName;
     cin >> FileName;
-    calc(FileName);
+    cout << calc(FileName) << endl;
+    MyTest temp;
+    temp.Test();
     return 0;
 }
